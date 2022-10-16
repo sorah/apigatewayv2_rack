f80f5e8ea29f9a2d629a3381295646278a266553
Sorah Fukumori <her@sorah.jp>
2022-10-17 07:46:15 +0900

    init

---
+++ lib/apigatewayv2_rack.rb
@@ -0,0 +1,8 @@
+# frozen_string_literal: true
+
+require_relative "apigatewayv2_rack/version"
+
+module Apigatewayv2Rack
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ spec/apigatewayv2_rack_spec.rb
@@ -0,0 +1,11 @@
+# frozen_string_literal: true
+
+RSpec.describe Apigatewayv2Rack do
+  it "has a version number" do
+    expect(Apigatewayv2Rack::VERSION).not_to be nil
+  end
+
+  it "does something useful" do
+    expect(false).to eq(true)
+  end
+end
