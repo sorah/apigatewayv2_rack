1a858b69f15415aed7b97346bfc1bc12d43bf7c0
Sorah Fukumori <her@sorah.jp>
2023-03-24 03:46:37 +0900

    handle_request now takes block

--- lib/apigatewayv2_rack.rb
+++ lib/apigatewayv2_rack.rb
@@ -8,9 +8,14 @@ require_relative "apigatewayv2_rack/response"
 module Apigatewayv2Rack
   # Takes Rack +app+, Lambda +event+ and +context+ of API Gateway V2 event and
   # returns a HTTP response from +app+ as API Gateway V2 Lambda event format.
-  def self.handle_request(app:, event:, context:, request_options: {})
+  #
+  # When block is given, converted Rack env will be passed to make some final
+  # modification before passing it to an +app+.
+  def self.handle_request(app:, event:, context:, request_options: {}, &block)
     req = Request.new(event, context, **request_options)
-    status, headers, body = app.call(req.to_h)
+    env = req.to_h
+    block&.call(env)
+    status, headers, body = app.call(env)
     Response.new(status: status, headers: headers, body: body, elb: req.elb?, multivalued: req.multivalued?).as_json
   end
 
