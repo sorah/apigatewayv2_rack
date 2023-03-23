2df61facf7dd34574bf20eda3719d26d9d9ec24e
Sorah Fukumori <her@sorah.jp>
2023-03-24 04:25:36 +0900

    propagate block

--- lib/apigatewayv2_rack.rb
+++ lib/apigatewayv2_rack.rb
@@ -17,26 +17,29 @@ module Apigatewayv2Rack
   def self.handle_request(app:, event:, context:, request_options: {}, &block)
     req = Request.new(event, context, **request_options)
     env = req.to_h
-    block&.call(env)
+    block&.call(env, req)
     status, headers, body = app.call(env)
     Response.new(status: status, headers: headers, body: body, elb: req.elb?, multivalued: req.multivalued?).as_json
   end
 
   module Handler
-    attr_reader :app 
-    def handle(event:, context:)
-      Apigatewayv2Rack.handle_request(event: event, context: context, app: @app)
+    attr_reader :app
+    attr_reader :block
+    def handle(event:, context:, &givenblock)
+      b = givenblock || @block
+      Apigatewayv2Rack.handle_request(event: event, context: context, app: @app, &b)
     end
   end
 
-  def self.generate_handler(app)
+  def self.generate_handler(app, &block)
     m = Module.new
     m.extend(Handler)
     m.instance_variable_set(:@app, app)
+    m.instance_variable_set(:@block, block)
     m
   end
 
-  def self.handler_from_rack_config_file(path = './config.ru')
+  def self.handler_from_rack_config_file(path = './config.ru', &block)
     require 'rack'
     require 'rack/builder'
     app = if Rack.release[0] == '2'
@@ -44,6 +47,6 @@ module Apigatewayv2Rack
     else
       Rack::Builder.load_file(path)
     end
-    generate_handler(app)
+    generate_handler(app, &block)
   end
 end
